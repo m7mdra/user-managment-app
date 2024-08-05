4cf16c1aab98771168a093a6741d15102f412874
Mohamed Elshiekh <xm7mdrax@gmail.com>
2024-08-05 11:40:36 +0300

    Delete app/src/main/java/com/example/usermanagmentapp/extension/isValidEmail.kt

--- app/src/main/java/com/example/usermanagmentapp/extension/isValidEmail.kt
+++
@@ -1,9 +0,0 @@
-package com.example.usermanagmentapp.extension
-
-import android.util.Patterns
-
-/**
- * Returns `true` if the string is a valid email address.
- */
-fun CharSequence?.isValidEmail() =
-    !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
